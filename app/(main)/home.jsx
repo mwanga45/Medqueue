d4d2327a8cf318de0a9e8c232e35dbb5e21d78b5
Lynxprazo <issamwanga02@gmail.com>
2025-02-20 18:11:24 +0300

    more addition

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import QuickAction from "../component/QuickAction";
 import { View, Text, StyleSheet } from "react-native";
 
 const Homepage = () => {
   return (
     <View style={styles.HomepageContainer}>
-      <View style ={styles.Homepagecover1}>
-        <Text >Hello world</Text>
+      <View style={styles.Homepagecover1}>
+        <Icon name="hospital-symbol" size={100} style={{color:"silver"}}></Icon>
       </View>
-      <View></View>
+      <Text style={styles.TextQuick}>Quick Action</Text>
+      <View style={styles.Homepagecover2}>
+        <View style={{ flex: 1, padding: 10 }}>
+          <QuickAction
+            name={"bell"}
+            size={30}
+            text={"Set Remainder"}
+            backgroundColor={"#274b5f"}
+          />
+        </View>
+        <View style={{ flex: 1, padding: 10 }}>
+          <QuickAction
+            name={"book"}
+            size={30}
+            text={"Booking"}
+            backgroundColor={"#00d4ff"}
+          />
+        </View>
+     
+      </View>
     </View>
   );
 };
@@ -18,19 +39,33 @@ const styles = StyleSheet.create({
   HomepageContainer: {
     flex: 1,
     backgroundColor: "#F7F7F7",
-    paddingLeft:1,
-    paddingRight:1,
-    paddingBottom:1
+    paddingLeft: 1.5,
+    paddingRight: 2,
+    paddingBottom: 1,
   },
-  Homepagecover1:{
+  Homepagecover1: {
+    position: "sticky",
+    justifyContent: "center",
+    borderBottomLeftRadius: "45px",
+    borderBottomRightRadius: "45px",
+    height: "50%",
+    width: "cover",
+    alignItems: "center",
+    backgroundColor: "#275f38",
+    boxShadow:
+      "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+    zIndex: 100,
+  },
+  TextQuick: {
+    fontSize: "25px",
+    fontWeight: 900,
+    fontFamily: "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+    margin: "12px",
+  },
+  Homepagecover2: {
+    flexDirection: "row",
+    width: "100%",
     justifyContent:"center",
-    borderRadius:"15px",
-    height:"50%",
-    width:"cover",
-    alignItems:"center",
-    backgroundColor:"#338117",
-    boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
-    
-
-  }
+    alignContent:"center"
+  },
 });
---
+++ app/component/QuickAction.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
+const QuickAction = ({backgroundColor, text , size , name, }) => {
+  return (
+    <View style ={[Quickstyles.Quickcontainer,{backgroundColor}]}>
+          <Icon size ={size} name = {name}  style={{color:"white"}}/>
+         <Text style={{color:"white"}}>{text}</Text>
+    </View>
+  );
+}
+
+export default QuickAction;
+
+
+const Quickstyles = StyleSheet.create({
+  Quickcontainer:{
+    display:"flex",
+    width:"100%",
+    height:"10svh",
+    padding:"12px",
+    margin:"4px",
+    color:"white",
+    borderRadius:"15px",
+    boxShadow:
+    "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+
+  }
+})
