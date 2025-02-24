c44c8a70854963a5b796ed0235b931e586416c46
Lynxprazo <issamwanga02@gmail.com>
2025-02-24 20:18:47 +0300

    dkcard

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -6,14 +6,15 @@ import Dktcard from "../component/doctorcard";
 export default function TabTwoScreen() {
   return (
     <View style= {teststyle.maintest}>
-        <Dktcard specialist={"Neurologist"} name={"Mandari"}/>
+        <Dktcard specialist={"Neurologist"} name={"Paul solm"}/>
     </View>
     )
   }
   const teststyle = StyleSheet.create({
     maintest:{
+      marginTop:200,
       backgroundImage:"white",
       justifyContent:"center",
-      alignContent:"center"
+      alignItems:"center"
     }
   })
--- app/component/doctorcard.jsx
+++ app/component/doctorcard.jsx
@@ -1,63 +1,73 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-
-const Dktcard = ({specialist,name}) => {
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+const Dktcard = ({ specialist, name }) => {
   return (
     <LinearGradient
-    colors={["rgba(0,36,2,1)", "rgba(19,213,84,1)", "rgba(3,234,25,1)"]}
-    start={{ x: 0.2, y: 0 }}
-    end={{ x: 1, y: 1 }}
-    style= {stylescard.Cardcontainer}
-  >
-    <View>
-        <View style= {stylescard.handlespecialist}>
-       <Text style= {stylescard.Specialist}>{specialist}</Text>     
+      colors={["rgba(0,36,2,1)", "rgba(19,213,84,1)", "rgba(3,234,25,1)"]}
+      start={{ x: 0.2, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={stylescard.Cardcontainer}
+    >
+      <View style = {stylescard.cardsides}>
+        <View>
+          <View style={stylescard.handlespecialist}>
+            <Text style={stylescard.Specialist}>{specialist}</Text>
+          </View>
+          <View style={stylescard.handlename}>
+            <Text style={stylescard.dktname}numberOfLines={1} ellipsizeMode={"middle"}>Dr.{name}</Text>
+          </View>
         </View>
-        <View style = {stylescard.handlename}>
-            <Text style={stylescard.dktname}>Dr.{name}</Text>
+        <View>
+            <Image source={require("../../assets/images/adaptive-icon.png")}
+            style= {stylescard.image}
+            />
         </View>
-    </View>
+      </View>
     </LinearGradient>
   );
-}
+};
 
 const stylescard = StyleSheet.create({
-    Cardcontainer:{
-        flex:1,
-        backgroundColor:"#d0f0c0",
-        width: "95%",
-        padding:12,
-        borderRadius:15
+  Cardcontainer: {
+    flex: 1,
+    backgroundColor: "#d0f0c0",
+    width: "95%",
+    padding: 12,
+    borderRadius: 15,
+  },
+  image:{
+    width: 100,
+    height: 100,
+    resizeMode: "contain", // Adjusts image scaling mode
 
+  },
+//   always view component is flex so there is no need to write flex
+  cardsides: {
+    justifyContent:"space-between",
+    flexDirection:"row"
+  },
+  handlespecialist: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.1)",
+    width: 100,
+    borderRadius: 4,
+    marginBottom: 12,
+  },
+  handlename: {
+    width: 200,
+    borderRadius: 4,
+  },
 
-    },
-    handlespecialist:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"rgba(0,0,0,0.2)",
-        width:"20%",
-        borderRadius:4,
-        marginBottom:12
-    },
-    handlename:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        width:"40%",
-        borderRadius:4,
-        paddingBottom:12,
-    },
-
-    dktname:{
-       color:"white",
-       fontSize:20,
-    },
-    Specialist:{
-       color:"white",
-       fontSize:10,
-    }
-})
+  dktname: {
+    color: "white",
+    fontSize: 20,
+  },
+  Specialist: {
+    color: "white",
+    fontSize: 10,
+  },
+});
 
 export default Dktcard;
