a43411e9a7ac72d49633bf42f7f9f16168d474ba
Lynxprazo <issamwanga02@gmail.com>
2025-02-25 14:40:06 +0300

    more changes

--- app/component/navigation.jsx
+++ app/component/navigation.jsx
@@ -5,9 +5,10 @@ const Navigationbar = () => {
   return (
     <View style= {navstyles.navcontainer}>
       <View style = {navstyles.mainnavcontainer}>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
+      <Text style= {{color:"white"}}>Home</Text>
+      <Text style= {{color:"white"}}>Home</Text>
+      <Text style= {{color:"white"}}>Home</Text>
+    
       </View>
     </View>
   );
@@ -17,21 +18,22 @@ const Navigationbar = () => {
     //    backgroundColor:"rgba(0, 0, 0, 0.2)",
        width:"100%",
        paddingHorizontal:12,
-       height:40,
+       height:60,
        justifyContent:"center",
        position:"relative",
-       top:300
+       top:283
 
 
    
     },
     mainnavcontainer:{
        alignItems:"center",
-       height:40,
+       height:60,
        justifyContent:"space-between",
        flexDirection:"row", 
        backgroundColor:"rgba(23, 22, 22, 0.9)",  
-       borderRadius:12
+       borderRadius:12,
+       paddingHorizontal:12,
     },
     
 
