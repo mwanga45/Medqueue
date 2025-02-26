c3e17ba2524ce44eff9c5b250bf17d996a3dc67d
Lynxprazo <issamwanga02@gmail.com>
2025-02-26 15:56:37 +0300

    more changes

---
+++ app/(main)/booking.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Booking = () => {
+  return (
+    <View>
+      <Text>
+          it time to booking page
+      </Text>
+    </View>
+  );
+}
+
+export default Booking;
