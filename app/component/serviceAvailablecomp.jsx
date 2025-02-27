2cd56cdfc59009aa037a336a5e5e26027942ea8c
Lynxprazo <issamwanga02@gmail.com>
2025-02-27 11:40:31 +0300

    more changes

--- app/component/serviceAvailablecomp.jsx
+++ app/component/serviceAvailablecomp.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Searchcomp from "../component/searchcomp"
 
 const ServiceAvailablecomp = () => {
   return (
-    <View>
-      <Text>hellow</Text>
+    <View style = {servicecomp.servcontainer}>
+         <View style = {servicecomp.searchcompAva}></View>
+         <View>
+            <Searchcomp/>
+         </View>
     </View>
   );
 }
+const servicecomp = StyleSheet.create({
+ servcontainer:{
+  backgroundColor:"grey",
+  height:300,
+  width:"100%"
+ }
+})
 
 export default ServiceAvailablecomp;
