0d46218aff7e6050d574d180b34c485b6bb0f655
Lynxprazo <issamwanga02@gmail.com>
2025-02-27 09:34:03 +0300

    more changes

--- app/(main)/booking.jsx
+++ app/(main)/booking.jsx
@@ -1,28 +1,54 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Navigationbar from "../component/navigation";
-import Icon from "react-native-vector-icons/FontAwesome5"
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const Booking = () => {
   return (
     <View style={bookingstyles.maincontainer}>
       <View style={bookingstyles.bkcontainer}>
-        <View style = {bookingstyles.Dperiod}>
-           <View style = {bookingstyles.period}><Text style= {bookingstyles.Dperiodname}>Morning</Text></View>
-           <View style = {bookingstyles.period}><Text style= {bookingstyles.Dperiodname}>Afternoon</Text></View>
-           <View style = {bookingstyles.period}><Text  style= {bookingstyles.Dperiodname}>Evening</Text></View>
-           <View style = {bookingstyles.period}><Text  style= {bookingstyles.Dperiodname}>Night</Text></View>
+        <View style={bookingstyles.Dperiod}>
+          <View style={bookingstyles.period}>
+            <Text style={bookingstyles.Dperiodname}>Morning</Text>
+          </View>
+          <View style={bookingstyles.period}>
+            <Text style={bookingstyles.Dperiodname}>Afternoon</Text>
+          </View>
+          <View style={bookingstyles.period}>
+            <Text style={bookingstyles.Dperiodname}>Evening</Text>
+          </View>
+          <View style={bookingstyles.period}>
+            <Text style={bookingstyles.Dperiodname}>Night</Text>
+          </View>
         </View>
-        <View style= {bookingstyles.bookng}>
-             <View>
-                <View style={bookingstyles.descriptiontxt}>
-                    <Icon name="calendar" size={20}></Icon>
-                    <Text style = {{color:"grey",fontSize:16}}>Select the meeting date</Text>
-                </View>
-                <View></View>
-             </View>
+        <View style={bookingstyles.bookng}>
+          <View>
+            <View style={bookingstyles.descriptiontxt}>
+              <Icon name="calendar" size={20} style={{ color: "grey" }} />
+              <Text style={{ color: "grey", fontSize: 16 }}>
+                Select the meeting date
+              </Text>
+            </View>
+            <View style={bookingstyles.Datebooking}>
+              <View>
+                <Text>2</Text>
+                <Text>Mon</Text>
+              </View>
+              <View>
+                <Text>2</Text>
+                <Text>Mon</Text>
+              </View>
+              <View>
+                <Text>2</Text>
+                <Text>Mon</Text>
+              </View>
+              <View>
+                <Text>2</Text>
+                <Text>Mon</Text>
+              </View>
+            </View>
+          </View>
         </View>
-
       </View>
       <Navigationbar />
     </View>
@@ -30,39 +56,44 @@ const Booking = () => {
 };
 
 const bookingstyles = StyleSheet.create({
-  descriptiontxt:{
-    flexDirection:"row",
-    justifyContent:"flex-start",
-    alignItems:"center",
+  Datebooking: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     columnGap:12
-
   },
-  bookng:{
-   width:"94%",
-   backgroundColor:"white",
-   paddingHorizontal:4,
-   borderRadius:23,
-   paddingHorizontal:12,
-   paddingVertical:12
+  descriptiontxt: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    columnGap: 12,
+    marginBottom: 12,
   },
-  Dperiodname:{
-    fontSize:17
+  bookng: {
+    width: "94%",
+    backgroundColor: "white",
+    paddingHorizontal: 4,
+    borderRadius: 23,
+    paddingHorizontal: 12,
+    paddingVertical: 12,
   },
-  period:{
-    backgroundColor:"white",
-    width:90,
-    height:35,
-    justifyContent:"center",
-    alignItems:"center",
-    borderRadius:24,
-    marginBottom:12,
-
+  Dperiodname: {
+    fontSize: 17,
   },
-  Dperiod:{
-    flexDirection:"row",
-    justifyContent:"space-evenly",
-    alignItems:'center',
-    width:"100%"
+  period: {
+    backgroundColor: "white",
+    width: 90,
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 24,
+    marginBottom: 12,
+  },
+  Dperiod: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
   },
   maincontainer: {
     flex: 1,
@@ -73,10 +104,8 @@ const bookingstyles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
-    marginBottom:89
-
+    marginBottom: 89,
   },
-
 });
 
 export default Booking;
