6f14e77c68a1681ab7572609312db1a73a1bf3a3
Lynxprazo <issamwanga02@gmail.com>
2025-02-27 15:30:34 +0300

    more changes

--- app/component/serviceAvailablecomp.jsx
+++ app/component/serviceAvailablecomp.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Searchcomp from "../component/searchcomp"
+import { Picker } from '@react-native-picker/picker';
 
 const ServiceAvailablecomp = () => {
+const [dropvalue, setdropvalue] = useState("position1")
   return (
     <View style = {servicecomp.servcontainer}>
          <View style = {servicecomp.searchcompAva}></View>
@@ -10,12 +12,30 @@ const ServiceAvailablecomp = () => {
          <View style = {servicecomp.searchsevcomp}>
             <Searchcomp placeholder={"search for service"}/>
          </View>
+         <View style ={servicecomp.pickercontainer}>
+          <Picker onValueChange={(index, positon) => setdropvalue(positon)} selectedValue={dropvalue} style= {servicecomp.pickersev}>
+            <Picker.Item label='choose service ' value="position1"/>
+            <Picker.Item label='service2' value="position2"/>
+            <Picker.Item label='service3' value="position3"/>
+            <Picker.Item label='service4' value="position4"/>
+          </Picker>
+         </View>
     </View>
   );
 }
 const servicecomp = StyleSheet.create({
+  pickersev:{
+    backgroundColor:"white",
+    justifyContent:"center",
+    alignItems:"center",
+    marginHorizontal:12
+  },
+  pickercontainer:{
+    // backgroundColor:"white",
+  },
   searchsevcomp:{
-     paddingHorizontal:12
+     paddingHorizontal:12,
+     marginBottom:8,
   },
  Titlessrvice:{
   justifyContent:"center",
@@ -28,7 +48,7 @@ const servicecomp = StyleSheet.create({
   width:"100%",
  },
  searchcompAva:{
-   paddingVertical:20
+   paddingVertical:20,
  }
 })
 
