513629bff86ffff9d1820d5a4406eeada3d55e03
Lynxprazo <issamwanga02@gmail.com>
2025-03-01 09:23:38 +0300

    more changes

--- app/(main)/booking.jsx
+++ app/(main)/booking.jsx
@@ -1,164 +1,213 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
-import {useRouter} from "expo-router"
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
 import ServiceAvailablecomp from "../component/serviceAvailablecomp";
 import Navigationbar from "../component/navigation";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Booking = () => {
-  useEffect(()=>{
-    handletimeslot()
-  },[]);
-  const [Timeslot, setTimeSlots]=useState([]);
-  const handletimeslot =()=>{
+  useEffect(() => {
+    handletimeslot();
+  }, []);
+  const [Timeslot, setTimeSlots] = useState([]);
+  const handletimeslot = () => {
     const slot = [];
     const startTime = new Date();
-    startTime.setHours(6,30,0,0);
+    startTime.setHours(6, 30, 0, 0);
     const endTime = new Date();
-    endTime.setHours(8,30,0,0);
+    endTime.setHours(8, 30, 0, 0);
 
     const currentTime = new Date(startTime);
 
-    while(currentTime <= endTime){
-      slot.push(currentTime.toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"}))
+    while (currentTime <= endTime) {
+      slot.push(
+        currentTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
       currentTime.setMinutes(currentTime.getMinutes() + 10);
     }
-    setTimeSlots(slot)
+    setTimeSlots(slot);
+  };
+  const router = useRouter();
+  const [bgcolor, setbgcolor] = useState({});
+  const [selectedTime, setselectedTime] = useState({});
 
-  }
-    const router = useRouter();
-    const [bgcolor , setbgcolor] = useState({});
-    const [selectedTime , setselectedTime] = useState({});
+  const handleSelectedslot = (index) => {
+    setselectedTime((prev) => {
+      if (prev[index]) {
+        return {};
+      }
+      return { [index]: true };
+    });
+  };
 
-    const handleSelectedslot= (index)=>{
-       setselectedTime((prev)=>{
-        if(prev[index]){
-          return{}
-        }
-        return{[index]:true}
-       })
-    }
+  const handledateday = (index) => {
+    setbgcolor((prev) => {
 
-    
-    const handledateday = (index)=>{
-      setbgcolor((prev) => {
-        // ...prev , [index]:!prev[index]
-        if (prev[index]) {
-          return {};
-        }
-        // Otherwise, set only this button as selected
-        return { [index]: true };
-      })
-    }
-    const getDayName = (date) => date.toLocaleString("en-Us", {weekday:"short"});
-    const getDayNumber = (date) => date.getDate();
+      if (prev[index]) {
+        return {};
+      }
+      return { [index]: true };
+    });
+  };
+  const getDayName = (date) =>
+    date.toLocaleString("en-Us", { weekday: "short" });
+  const getDayNumber = (date) => date.getDate();
 
-    const today = new Date()
-    const weekday = Array.from({length:4}, (_,index) =>{
-      const newDate = new Date(today);
-      newDate.setDate(today.getDate() +index);
-      return newDate;
-
-    } )
+  const today = new Date();
+  const weekday = Array.from({ length: 4 }, (_, index) => {
+    const newDate = new Date(today);
+    newDate.setDate(today.getDate() + index);
+    return newDate;
+  });
   return (
     <View style={bookingstyles.maincontainer}>
       <ScrollView>
-      <View style={bookingstyles.bkcontainer}>
-        <Ionicons
-          name="medical"
-          style={{ marginBottom: 10 }}
-          size={90}
-          color="#f0f0f0"
-        />
-        <View>
-          <ServiceAvailablecomp/>
-        </View>
-        <View style={bookingstyles.suggestiontitle}>
-          <Text style={{ color: "#f0f0f0", fontSize: 20, fontWeight: 700 }}>Select Day Period For Qiuck Suggestion</Text>
-        </View>
-        <View style={bookingstyles.Dperiod}>
-          <View style={bookingstyles.period}>
-            <Text style={bookingstyles.Dperiodname}>Morning</Text>
-          </View>
-          <View style={bookingstyles.period}>
-            <Text style={bookingstyles.Dperiodname}>Afternoon</Text>
-          </View>
-          <View style={bookingstyles.period}>
-            <Text style={bookingstyles.Dperiodname}>Evening</Text>
-          </View>
-          <View style={bookingstyles.period}>
-            <Text style={bookingstyles.Dperiodname}>Night</Text>
-          </View>
-        </View>
-        <View style={bookingstyles.bookng}>
+        <View style={bookingstyles.bkcontainer}>
+          <Ionicons
+            name="medical"
+            style={{ marginBottom: 8, marginTop:30 }}
+            size={90}
+            color="#f0f0f0"
+          />
           <View>
-            <View style={bookingstyles.descriptiontxt}>
-              <Icon name="calendar" size={20} style={{ color: "#f0f0f0" }} />
-              <Text style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}>
-                Select the meeting date
-              </Text>
+            <ServiceAvailablecomp />
+          </View>
+          <View style={bookingstyles.suggestiontitle}>
+            <Text style={{ color: "#f0f0f0", fontSize: 20, fontWeight: 700 }}>
+              Select Day Period For Qiuck Suggestion
+            </Text>
+          </View>
+          <View style={bookingstyles.Dperiod}>
+            <View style={bookingstyles.period}>
+              <Text style={bookingstyles.Dperiodname}>Morning</Text>
             </View>
-            <View style={bookingstyles.Datebooking}>
-            {weekday.map((day,index) =>(
-              <TouchableOpacity onPress={()=> handledateday(index)} key={index}>
-              <View style={[bookingstyles.dateday, bgcolor[index] && bookingstyles.bgcolor]}>
-                <Text style={bookingstyles.dayNum}>{getDayNumber(day)}</Text>
-                <Text style={bookingstyles.dayName}>{getDayName(day)}</Text>
+            <View style={bookingstyles.period}>
+              <Text style={bookingstyles.Dperiodname}>Afternoon</Text>
+            </View>
+            <View style={bookingstyles.period}>
+              <Text style={bookingstyles.Dperiodname}>Evening</Text>
+            </View>
+            <View style={bookingstyles.period}>
+              <Text style={bookingstyles.Dperiodname}>Night</Text>
+            </View>
+          </View>
+          <View style={bookingstyles.bookng}>
+            <View>
+              <View style={bookingstyles.descriptiontxt}>
+                <Icon name="calendar" size={20} style={{ color: "#f0f0f0" }} />
+                <Text
+                  style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}
+                >
+                  Select the meeting date
+                </Text>
               </View>
-              </TouchableOpacity>))}
-            </View>
-            <View style={bookingstyles.timecontainer}>
-              <Ionicons name="timer" size={20} style={{ color: "#f0f0f0" }} />
-              <Text style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}>
-                Select time slot
-              </Text>
-            </View>
-            <ScrollView
-              horizontal={true}
-              style={bookingstyles.scrollslot}
-              showsHorizontalScrollIndicator={true}
-              persistentScrollbar={true}
-            >  {Timeslot.map((time,index)=>(
-              <View style={bookingstyles.slottmt} key={index}>
-                <TouchableOpacity onPress={()=> handleSelectedslot(index)}>
-                <View style={[bookingstyles.slot ,selectedTime[index] && bookingstyles.bgcolor]}>
-                  <Text style={{ color: "grey", fontSize: 18, fontWeight: 600 }}>{time}</Text>
-                </View>
-                </TouchableOpacity>
-              </View>))}
-            </ScrollView>
-            <View style = {bookingstyles.bookngcontainer}>
-                  <TouchableOpacity onPress={()=>router.push("booking")} style={bookingstyles.bookngbtn}>
-                    <Text style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}>Book Now</Text>
+              <View style={bookingstyles.Datebooking}>
+                {weekday.map((day, index) => (
+                  <TouchableOpacity
+                    onPress={() => handledateday(index)}
+                    key={index}
+                  >
+                    <View
+                      style={[
+                        bookingstyles.dateday,
+                        bgcolor[index] && bookingstyles.bgcolor,
+                      ]}
+                    >
+                      <Text style={bookingstyles.dayNum}>
+                        {getDayNumber(day)}
+                      </Text>
+                      <Text style={bookingstyles.dayName}>
+                        {getDayName(day)}
+                      </Text>
+                    </View>
                   </TouchableOpacity>
-                </View>
+                ))}
+              </View>
+              <View style={bookingstyles.timecontainer}>
+                <Ionicons name="timer" size={20} style={{ color: "#f0f0f0" }} />
+                <Text
+                  style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}
+                >
+                  Select time slot
+                </Text>
+              </View>
+              <ScrollView
+                horizontal={true}
+                style={bookingstyles.scrollslot}
+                showsHorizontalScrollIndicator={true}
+                persistentScrollbar={true}
+              >
+                {Timeslot.map((time, index) => (
+                  <View style={bookingstyles.slottmt} key={index}>
+                    <TouchableOpacity onPress={() => handleSelectedslot(index)}>
+                      <View
+                        style={[
+                          bookingstyles.slot,
+                          selectedTime[index] && bookingstyles.bgcolor,
+                        ]}
+                      >
+                        <Text
+                          style={{
+                            color: "grey",
+                            fontSize: 18,
+                            fontWeight: 600,
+                          }}
+                        >
+                          {time}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                ))}
+              </ScrollView>
+              <View style={bookingstyles.bookngcontainer}>
+                <TouchableOpacity
+                  onPress={() => router.push("booking")}
+                  style={bookingstyles.bookngbtn}
+                >
+                  <Text
+                    style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}
+                  >
+                    Book Now
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         </View>
-      </View>
       </ScrollView>
-      <Navigationbar/>
+      <Navigationbar />
     </View>
   );
 };
 
 const bookingstyles = StyleSheet.create({
-  bgcolor:{
-    backgroundColor:"#51bdea"
+  bgcolor: {
+    backgroundColor: "#51bdea",
   },
-  bookngbtn:{
-     backgroundColor:"white",
-     height:50,
-     width:200,
-     justifyContent:"center",
-     alignItems:"center",
-     borderRadius:23,
-     backgroundColor: "rgba(0,0,0,0.8)",
+  bookngbtn: {
+    backgroundColor: "white",
+    height: 50,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 23,
+    backgroundColor: "rgba(0,0,0,0.8)",
   },
-  bookngcontainer:{
-     marginTop:12,
-     justifyContent:"center",
-     alignItems:"center"
+  bookngcontainer: {
+    marginTop: 12,
+    justifyContent: "center",
+    alignItems: "center",
   },
   scrollslot: {
     paddingHorizontal: 1,
@@ -172,7 +221,7 @@ const bookingstyles = StyleSheet.create({
     borderRadius: 13,
   },
   slottmt: {
-    justifyContent:"space-evenly",
+    justifyContent: "space-evenly",
     alignItems: "center",
     flexDirection: "row",
     columnGap: 32,
@@ -230,8 +279,8 @@ const bookingstyles = StyleSheet.create({
     fontWeight: 800,
     color: "grey",
   },
-  suggestiontitle:{
-    marginVertical:12
+  suggestiontitle: {
+    marginVertical: 12,
   },
   period: {
     backgroundColor: "white",
@@ -258,7 +307,6 @@ const bookingstyles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
     marginBottom: 89,
-
   },
 });
 
