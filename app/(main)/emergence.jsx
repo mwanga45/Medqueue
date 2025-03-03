04d0b9956a5642581800c2e4468ec4a95b2fc161
Lynxprazo <issamwanga02@gmail.com>
2025-03-03 09:47:53 +0300

    more changes

--- app/(main)/emergence.jsx
+++ app/(main)/emergence.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Navigationbar from "../component/navigation";
 import { ScrollView } from "react-native";
+import {UserProf} from "../component/userprofile"
 
 const Emergence = () => {
   return (
     <View style={emergencestyles.maincontainer}>
-      <View style={emergencestyles.mapcontainer}>
+      {/* <View style={emergencestyles.mapcontainer}>
         <Text>Map view</Text>
-      </View>
+      </View> */}
       <ScrollView>
-        <View>
-          <Text>Emergence-type</Text>
+        <View style = {emergencestyles.emergencesection}>
+          <View>
+             <Text style={{color:"white", fontSize:18, fontWeight:600, }}>Jonathan Methew</Text>
+             <Text style={{color:"white", fontSize:18, fontWeight:600, }}>Age 26</Text>
+          </View>
+          <View>
+             {/* <Text style={{color:"white", fontSize:18, fontWeight:600, }}>Jonathan Methew</Text>
+             <Text style={{color:"white", fontSize:18, fontWeight:600, }}>Age 26</Text> */}
+             <UserProf/>
+          </View>
         </View>
       </ScrollView>
       <Navigationbar />
@@ -19,16 +28,25 @@ const Emergence = () => {
   );
 };
 const emergencestyles = StyleSheet.create({
-  mapcontainer: {
-    width:"60%"
+  emergencesection:{
+    flex:1,
+    backgroundColor:"rgba(0,0,0,0.9)",
+    borderRadius:12,
+    paddingHorizontal:12,
+    flexDirection:"row",
+    columnGap:12
+
   },
+  // mapcontainer: {
+  //   width:"50%"
+  // },
   maincontainer: {
     flex: 1,
     flexDirection:"row",
     justifyContent:"flex-start",
     alignItems:"flex-start",
     backgroundColor: "white",
-    paddingHorizontal:12,
+    paddingHorizontal:1,
     paddingVertical:34
 
   },
