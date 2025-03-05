78cb0c70bb445709520a34711120b1c7d6addb54
Lynxprazo <issamwanga02@gmail.com>
2025-03-05 10:06:10 +0300

    more changes

--- app/(main)/emergence.jsx
+++ app/(main)/emergence.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet,Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import Navigationbar from "../component/navigation";
-import { ScrollView } from "react-native";
 import UserProf from "../component/userprofile";
 import MedicalButton from "../component/medicalbtn";
 
@@ -10,51 +9,70 @@ const Emergence = () => {
     <View style={emergencestyles.maincontainer}>
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <View style={emergencestyles.emergenceprofile}>
-          <UserProf  Age={26}/>
+          <UserProf Age={26} />
         </View>
-        
+
         <View style={emergencestyles.emergencesection}>
           <View style={emergencestyles.patientemergencyrec}>
             <View style={emergencestyles.transportmeans}>
-               <Text style = {emergencestyles.transporttitle}>Choose Transport means </Text>
-               <ScrollView showsHorizontalScrollIndicator={true} horizontal={true} persistentScrollbar={true}> 
-               <View  style = {emergencestyles.costandmeans}>
-                <View style = {emergencestyles.imagecontainer}>
-                <Image source={require("../../assets/images/adaptive-icon.png")} style={emergencestyles.image} />
+              <Text style={emergencestyles.transporttitle}>
+                Choose Transport Means
+              </Text>
+              <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator
+                persistentScrollbar
+                contentContainerStyle={{ flexDirection: "row" }}
+              >
+                <View style={emergencestyles.costandmeans}>
+                  <View style={emergencestyles.imagecontainer}>
+                    <Image
+                      source={require("../../assets/images/adaptive-icon.png")}
+                      style={emergencestyles.image}
+                    />
+                  </View>
+                  <View style={emergencestyles.innercontainer}>
+                    <Text style={emergencestyles.cost}>Helicopter</Text>
+                    <Text style={emergencestyles.cost}>Cost 0$</Text>
+                  </View>
                 </View>
-                <View  style = {emergencestyles.innercontainer}>
-                <Text style={emergencestyles.cost}>helcopter</Text>
-                <Text style={emergencestyles.cost}>cost 0$</Text>
+                <View style={emergencestyles.costandmeans}>
+                  <View style={emergencestyles.imagecontainer}>
+                    <Image
+                      source={require("../../assets/images/adaptive-icon.png")}
+                      style={emergencestyles.image}
+                    />
+                  </View>
+                  <View style={emergencestyles.innercontainer}>
+                    <Text style={emergencestyles.cost}>Helicopter</Text>
+                    <Text style={emergencestyles.cost}>Cost 0$</Text>
+                  </View>
                 </View>
-               </View>
-               <View  style = {emergencestyles.costandmeans}>
-                <View style = {emergencestyles.imagecontainer}>
-                <Image source={require("../../assets/images/adaptive-icon.png")} style={emergencestyles.image} />
+                <View style={emergencestyles.costandmeans}>
+                  <View style={emergencestyles.imagecontainer}>
+                    <Image
+                      source={require("../../assets/images/adaptive-icon.png")}
+                      style={emergencestyles.image}
+                    />
+                  </View>
+                  <View style={emergencestyles.innercontainer}>
+                    <Text style={emergencestyles.cost}>Helicopter</Text>
+                    <Text style={emergencestyles.cost}>Cost 0$</Text>
+                  </View>
                 </View>
-                <View  style = {emergencestyles.innercontainer}>
-                <Text style={emergencestyles.cost}>helcopter</Text>
-                <Text style={emergencestyles.cost}>cost 0$</Text>
-                </View>
-               </View>
-               </ScrollView>
-            </View>
-            <View>
-            </View>
-            <View>
-
+              </ScrollView>
             </View>
           </View>
-          
+
           <View style={{ flex: 1 }}>
             <ScrollView
-              showsVerticalScrollIndicator={true}
+              showsVerticalScrollIndicator
+              nestedScrollEnabled={true}
               contentContainerStyle={emergencestyles.patientservice}
             >
-            {
-              Array.from({length:40}).map((_,index)=>(
-                <MedicalButton key={index}/>
-              ))
-            }
+              {Array.from({ length: 40 }).map((_, index) => (
+                <MedicalButton key={index} />
+              ))}
             </ScrollView>
           </View>
         </View>
@@ -65,34 +83,32 @@ const Emergence = () => {
 };
 
 const emergencestyles = StyleSheet.create({
-  cost:{
-   fontWeight:400,
-   fontSize:18,
-  color:"grey"
+  cost: {
+    fontWeight: "400",
+    fontSize: 18,
+    color: "grey",
   },
-  innercontainer:{
-   justifyContent:"space-around",
-   alignItems:"center",
-   flexDirection:"row"
+  innercontainer: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
   },
-  costandmeans:{
-   backgroundColor:"white",
-   borderRadius:22,
-   marginVertical:12,
-   paddingVertical:8,
-   paddingHorizontal:8,
-   marginHorizontal:12,
-
-   
+  costandmeans: {
+    backgroundColor: "white",
+    borderRadius: 22,
+    marginVertical: 12,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    marginHorizontal: 12,
   },
-  transporttitle:{
-    fontSize:20,
-    fontWeight:600,
+  transporttitle: {
+    fontSize: 20,
+    fontWeight: "600",
   },
-  imagecontainer:{
-   justifyContent:"center",
-   alignItems:"center",
-   backgroundColor:"rgba(236, 236, 236, 0.2)",
+  imagecontainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(236, 236, 236, 0.2)",
   },
   image: {
     width: 90,
@@ -100,23 +116,22 @@ const emergencestyles = StyleSheet.create({
     resizeMode: "contain",
   },
   patientservice: {
-    position:"absolute",
-    right:0,
-    backgroundColor:"rgba(175, 185, 185, 0.2)",
-    width:102,
-    borderRadius:12,
+    backgroundColor: "rgba(175, 185, 185, 0.2)",
+    width: 102,
+    borderRadius: 12,
+    padding: 10,
   },
   patientemergencyrec: {
     width: "73%",
-    justifyContent:"center",
-    alignItems:"center",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 12,
-    position: "absolute", 
-    top:12,           
-    left: 0,
-    right: 0,
-    backgroundColor:"rgba(199, 248, 248, 0.2)",
-
+    backgroundColor: "rgba(199, 248, 248, 0.2)",
+    margin: 12,
+  },
+  // Added missing style for transportmeans
+  transportmeans: {
+    marginVertical: 10,
   },
   emergencesection: {
     flexDirection: "row",
@@ -128,6 +143,7 @@ const emergencestyles = StyleSheet.create({
     backgroundColor: "rgba(255,255,255,0.6)",
     borderRadius: 12,
     paddingHorizontal: 12,
+    margin: 12,
   },
   maincontainer: {
     flex: 1,
