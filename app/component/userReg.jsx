bc0d6582e122c40e3cd2c2da6965929d38ac4423
Lynxprazo <issamwanga02@gmail.com>
2025-03-12 13:54:48 +0300

    more formcompo change

--- app/component/userReg.jsx
+++ app/component/userReg.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"
 import {
   View,
   StyleSheet,
@@ -8,13 +7,30 @@ import {
   TextInput,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import axios from "axios";
 
-const UserReg = ({close}) => {;
+const UserReg = ({ close }) => {
+  const [Formstate, setFormstate] = useState({
+    fullname: "",
+    phone_num: "",
+    email_address:"",
+    home_address: "",
+  });
+  const handleTextchange = (fieldname , value)=>{
+    setFormstate((prevstate)=>({
+        ...prevstate,[fieldname]:value
+    }))
+  }
   return (
     <View style={reg.userregcontainer}>
       <View style={reg.close}>
         <TouchableOpacity>
-          <Icon name="times" size={24} color="rgb(74, 73, 73)" onPress={close} />
+          <Icon
+            name="times"
+            size={24}
+            color="rgb(74, 73, 73)"
+            onPress={close}
+          />
         </TouchableOpacity>
       </View>
       <View>
@@ -24,7 +40,8 @@ const UserReg = ({close}) => {;
       </View>
       <View style={reg.formregpart}>
         <TextInput
-          nativeID="name"
+          value={Formstate.fullname}
+          onChangeText={handleTextchange}
           placeholder="full name please"
           style={{
             height: 40,
@@ -33,11 +50,12 @@ const UserReg = ({close}) => {;
             paddingHorizontal: 10,
             width: 250,
             borderRadius: 10,
-            marginTop:30
+            marginTop: 30,
           }}
         />
         <TextInput
-          nativeID="name"
+          value={Formstate.phone_num}
+          onChangeText={handleTextchange}
           placeholder="Phone number eg 07......."
           keyboardType="numeric"
           style={{
@@ -47,11 +65,12 @@ const UserReg = ({close}) => {;
             paddingHorizontal: 10,
             width: 250,
             borderRadius: 10,
-            marginTop:30,
+            marginTop: 30,
           }}
         />
         <TextInput
-          nativeID="name"
+            value={Formstate.email_address}
+          onChangeText={handleTextchange}
           placeholder="email please"
           keyboardType="email-address"
           style={{
@@ -61,11 +80,12 @@ const UserReg = ({close}) => {;
             paddingHorizontal: 10,
             width: 250,
             borderRadius: 10,
-            marginTop:30
+            marginTop: 30,
           }}
         />
         <TextInput
-          nativeID="name"
+         value={Formstate.home_address}
+          onChangeText={handleTextchange}
           placeholder="home address if you have please"
           style={{
             height: 40,
@@ -74,11 +94,23 @@ const UserReg = ({close}) => {;
             paddingHorizontal: 10,
             width: 250,
             borderRadius: 10,
-            marginTop:30
+            marginTop: 30,
           }}
         />
-        <TouchableOpacity style={{backgroundColor:"rgb(35, 35, 35)", marginTop:32,height:39,width:200, justifyContent:"center", alignItems:"center",borderRadius:10,}}> 
-          <Text style={{color:"white", fontSize:18, letterSpacing:2}}>Register</Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "rgb(35, 35, 35)",
+            marginTop: 32,
+            height: 39,
+            width: 200,
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 18, letterSpacing: 2 }}>
+            Register
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -112,10 +144,10 @@ const reg = StyleSheet.create({
     alignItems: "flex-end",
     padding: 12,
   },
-  formregpart:{
-   justifyContent:"center",
-   alignItems:"center",
-   columnGap:12,
+  formregpart: {
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 12,
   },
 });
 export default UserReg;
