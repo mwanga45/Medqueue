ba2656d2241f912af68bd53d7bb27848ed14ed1c
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 11:00:04 +0300

    more changes

--- app/component/Viewdetails.jsx
+++ app/component/Viewdetails.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Modal,Image } from "react-native";
 
 const Viewdetails = () => {
   return (
-    <View>
-        
-    </View>
+    <Modal>
+      <View>
+        <View>
+            <Image source={require("../../assets/images/favicon.png")} resizeMethod="resize"></Image>
+        </View>
+        <View>
+          <Text>My details here</Text>
+          <Text>Username: </Text>
+          <Text>Phone_Number: </Text>
+          <Text>Home Address: </Text>
+        </View>
+      </View>
+    </Modal>
   );
-}
+};
 
 export default Viewdetails;
