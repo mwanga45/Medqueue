7b3d14d9ce0bf7361541c90e9b4943b1a66d73dc
Lynxprazo <issamwanga02@gmail.com>
2025-04-25 16:28:42 +0300

    more changes

--- app/booking.tsx
+++ app/booking.tsx
@@ -1,51 +1,122 @@
 import React from "react";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
-import { ScrollView , GestureHandlerRootView} from "react-native-gesture-handler";
+import {
+  ScrollView,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 const { height, width } = Dimensions.get("window");
 const Booking = () => {
   return (
-    <GestureHandlerRootView style ={{flex:1}}>
-    <SafeAreaView style={stylesbooking.container}>
-      <View style={stylesbooking.userprofile}></View>
-      <View style={stylesbooking.bookingcontainer}>
-        <View style={stylesbooking.bookingforcontainer}></View>
-        <View style={stylesbooking.bookingpage}>
-          <View style={stylesbooking.bookingpagedate}>
-            <Text style={stylesbooking.textdiscription}>
-              Select Date for the booking
-            </Text>
-            <View style={stylesbooking.dateslotecontainer}>
-              <View style={stylesbooking.dateslote}>
-                <Text style={stylesbooking.dateslotecontent}>2</Text>
-                <Text style={stylesbooking.dateslotecontent}>Day</Text>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={stylesbooking.container}>
+        <View style={stylesbooking.userprofile}></View>
+        <View style={stylesbooking.bookingcontainer}>
+          <View style={stylesbooking.bookingforcontainer}></View>
+          <View style={stylesbooking.bookingpage}>
+            <View style={stylesbooking.bookingpagedate}>
+              <Text style={stylesbooking.textdiscription}>
+                Select Date for the booking
+              </Text>
+              <View style={stylesbooking.dateslotecontainer}>
+                <View style={stylesbooking.dateslote}>
+                  <Text style={stylesbooking.dateslotecontent}>2</Text>
+                  <Text style={stylesbooking.dateslotecontent}>Day</Text>
+                </View>
+                <View style={stylesbooking.dateslote}>
+                  <Text style={stylesbooking.dateslotecontent}>3</Text>
+                  <Text style={stylesbooking.dateslotecontent}>Day</Text>
+                </View>
+                <View style={stylesbooking.dateslote}>
+                  <Text style={stylesbooking.dateslotecontent}>4</Text>
+                  <Text style={stylesbooking.dateslotecontent}>Day</Text>
+                </View>
+                <View style={stylesbooking.dateslote}>
+                  <Text style={stylesbooking.dateslotecontent}>5</Text>
+                  <Text style={stylesbooking.dateslotecontent}>Day</Text>
+                </View>
               </View>
-              <View style={stylesbooking.dateslote}>
-                <Text style={stylesbooking.dateslotecontent}>3</Text>
-                <Text style={stylesbooking.dateslotecontent}>Day</Text>
+            </View>
+            <View style={stylesbooking.bookingpagetime}>
+              <Text style={stylesbooking.textdiscription}>
+                {" "}
+                Select a time slot
+              </Text>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={true}
+                persistentScrollbar={true}
+                style={stylesbooking.scrollcontainer}
+              >
+                <TouchableOpacity>
+                  <View style={[stylesbooking.slot]}>
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 18,
+                        fontWeight: 600,
+                      }}
+                    >
+                      12:30-12:40
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View style={[stylesbooking.slot]}>
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 18,
+                        fontWeight: 600,
+                      }}
+                    >
+                      12:30-12:40
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View style={[stylesbooking.slot]}>
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 18,
+                        fontWeight: 600,
+                      }}
+                    >
+                      12:30-12:40
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </ScrollView>
+            </View>
+            <View style={stylesbooking.bookingpageprice}>
+              <View>
+                <Text style={stylesbooking.textdiscription}>Price</Text>
+                <Text style={{ color: "", fontSize: 28, fontWeight: "800" }}>
+                  2500 Tsh
+                </Text>
               </View>
-              <View style={stylesbooking.dateslote}>
-                <Text style={stylesbooking.dateslotecontent}>4</Text>
-                <Text style={stylesbooking.dateslotecontent}>Day</Text>
-              </View>
-              <View style={stylesbooking.dateslote}>
-                <Text style={stylesbooking.dateslotecontent}>5</Text>
-                <Text style={stylesbooking.dateslotecontent}>Day</Text>
+              <View>
+              <TouchableOpacity style={stylesbooking.bookngbtn}>
+                <Text
+                  style={{ color: "#f0f0f0", fontSize: 18, fontWeight: 600 }}
+                >
+                  Book Now
+                </Text>
+              </TouchableOpacity>
               </View>
             </View>
           </View>
-          <View style={stylesbooking.bookingpagetime}>
-            <Text style={stylesbooking.textdiscription}> Select a time slot</Text>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator= {true} persistentScrollbar={true}>
-
-            </ScrollView>
-          </View>
-          <View style={stylesbooking.bookingpageprice}></View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
 };
@@ -83,17 +154,20 @@ const stylesbooking = StyleSheet.create({
     borderRadius: 40,
     paddingVertical: 40,
     paddingHorizontal: 10,
-    rowGap:40,
+    rowGap: 40,
   },
   bookingpagedate: {
     width: "100%",
     flexDirection: "column",
-    rowGap:10,
+    rowGap: 10,
   },
-  bookingpagetime: {
-
+  bookingpagetime: {},
+  bookingpageprice: {
+    maxWidth:300,
+    flexDirection: "row",
+    width: "100%",
+    columnGap:width * 0.17
   },
-  bookingpageprice: {},
   textdiscription: {
     color: "grey",
     fontSize: 18,
@@ -111,10 +185,34 @@ const stylesbooking = StyleSheet.create({
     flexDirection: "row",
     columnGap: width * 0.043,
   },
+  slot: {
+    width: 150,
+    height: 50,
+    backgroundColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 13,
+    color: "white",
+    marginTop: 10,
+    marginHorizontal: 23,
+  },
+  scrollcontainer: {
+    backgroundColor: "#d3f4fe",
+    height: 80,
+  },
   dateslotecontent: {
     color: "white",
     fontSize: 20,
     fontWeight: "800",
+    borderRadius: 500,
+  },
+  bookngbtn: {
+    height: 50,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 23,
+    backgroundColor: "rgba(0,0,0,0.8)",
   },
 });
 
