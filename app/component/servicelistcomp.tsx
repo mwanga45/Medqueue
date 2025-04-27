f74d37d5e2b5b79068a52be5ead7e8a91a57f953
Lynxprazo <issamwanga02@gmail.com>
2025-04-27 18:39:37 +0300

    more changes

--- app/component/servicelistcomp.tsx
+++ app/component/servicelistcomp.tsx
@@ -1,83 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Alert, StyleSheet, Dimensions} from 'react-native';
-import axios from 'axios';
-import { ScrollView,GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import axios from "axios";
+import {
+  ScrollView,
+  GestureHandlerRootView,
+  TextInput,
+} from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
 
 const Servicelistcomp = () => {
-      const [service, setservice] = useState<[]|any>([]);
-    const handleGetService = async()=>{
-        try{
-         const res = await axios.get("http://192.168.110.251:8800/serviceAvailable")
-         if (!res.data.success){
-          Alert.alert(res.data.message)
-         }
-         const info = res.data
-         setservice(info.data)
-        }catch(err){
-          Alert.alert("something went wrong")
-          console.error(err)
-        }
+  const [service, setservice] = useState<[] | any>([]);
+  const handleGetService = async () => {
+    try {
+      const res = await axios.get(
+        "http://192.168.110.251:8800/serviceAvailable"
+      );
+      if (!res.data.success) {
+        Alert.alert(res.data.message);
       }
-      useEffect(()=>{
-        // handleGetService()
-      },[])
+      const info = res.data;
+      setservice(info.data);
+    } catch (err) {
+      Alert.alert("something went wrong");
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    // handleGetService()
+  }, []);
   return (
-    <GestureHandlerRootView style ={stylesmodal.container}>
+    <GestureHandlerRootView style={stylesmodal.container}>
       <View style={stylesmodal.headingpart}>
-        <Text style={stylesmodal.texttitle}>Here are the List of our Service</Text>
+        <Text style={stylesmodal.texttitle}>
+          Here are the List of our Service
+        </Text>
       </View>
-      <View style = {stylesmodal.searchpart}>
-        <View style = {stylesmodal.textinputcontainer}>
-            <TextInput/>
+      <View style={stylesmodal.searchpart}>
+        <View style={stylesmodal.textinputcontainer}>
+          <TextInput placeholder="search" style={stylesmodal.input} />
+          <TouchableOpacity>
+            <Ionicons name="search" color="grey" size={20} />
+          </TouchableOpacity>
         </View>
       </View>
-      <View style ={stylesmodal.listcontainer}></View>
+      <View style={stylesmodal.listcontainer}></View>
     </GestureHandlerRootView>
   );
-}
-const {height} = Dimensions.get('window')
+};
+const { height } = Dimensions.get("window");
 const stylesmodal = StyleSheet.create({
-  container:{
-    flex:1,
-    paddingHorizontal:10,
-    paddingVertical:4
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
   },
-  headingpart:{
-   height: height *0.2,
-   width:"100%",
-   backgroundColor:"rgb(19, 243, 12)",
-   justifyContent:"center",
-   alignItems:"center"
+  headingpart: {
+    height: height * 0.2,
+    width: "100%",
+    backgroundColor: "rgb(19, 243, 12)",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  searchpart:{
-   justifyContent:"center",
-   alignItems:"center",
-   height:height *0.08,
-   width:"100%",
-   backgroundColor:"red",
-   marginTop:height *0.02
+  searchpart: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: height * 0.08,
+    width: "100%",
+    backgroundColor: "red",
+    marginTop: height * 0.02,
   },
-  listcontainer:{
-    height:height *0.67,
-    width:"100%",
-    backgroundColor:"grey",
-    marginTop:height * 0.02
-
+  listcontainer: {
+    height: height * 0.67,
+    width: "100%",
+    backgroundColor: "grey",
+    marginTop: height * 0.02,
+    borderRadius: 25,
   },
-  texttitle:{
-    fontSize:20,
-    fontWeight:"800",
-    color:"white"
+  texttitle: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "white",
   },
-  textinputcontainer:{
-   backgroundColor:"white",
-   width:"90%",
-   height:50,
-   
-
-
-  }
-
-})
+  textinputcontainer: {
+    backgroundColor: "white",
+    width: "90%",
+    height: 50,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  input: {
+    width: "80%",
+    height: "100%",
+    fontWeight: "500",
+    fontSize: 19,
+    paddingHorizontal: 20,
+  },
+});
 
 export default Servicelistcomp;
