dda5eb05647f32d8f897ef1972eb56f2349de8b7
mwanga45 <issamwanga02@gmail.com>
2025-06-06 15:06:47 +0300

    more changes

--- app/login.tsx
+++ app/login.tsx
@@ -1,35 +1,82 @@
-import { View, Text, StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, KeyboardAvoidingView, Touchable, TouchableOpacity,Modal } from 'react-native'
+import React, { useState } from 'react'
+import UserRegistration from "./component/userRegistration";
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window')
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Ionicons } from "@expo/vector-icons";
 import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 export default function Login() {
+    const [isReg, setisReg] = useState<boolean>(true)
+      const [isClosed, setClosed] = useState(true);
+      const [isVerfy, setisVerfy] = useState<boolean|null>(null);
     return (
-        
-            <GestureHandlerRootView> 
-                <ScrollView style={styles.container}>
-                    <View style={styles.title}>
-                        <Text style={styles.text}>Hello welcome Again! <Icon name="smile" size={30} color /></Text>
+
+        <GestureHandlerRootView>
+            <ScrollView style={styles.container}>
+                <View style={styles.title}>
+                    <Text style={styles.text}>Hello welcome Again! <Icon name="smile" size={30} color /></Text>
+                </View>
+                <View style={styles.formcontainer}>
+                    <View style={styles.switchContainer}>
+                        <TouchableOpacity style={styles.switchBtn} onPress={() => setisReg(true)}>
+                            <Text>login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.switchBtn} onPress={() => setisReg(false)}>
+                            <Text>Register</Text>
+                        </TouchableOpacity>
                     </View>
-                    <View style={styles.formcontainer}>
-                        <KeyboardAvoidingView>
-                        <View>
-                            <TextInput></TextInput>
-                        </View>
-                        <View>
-                            <TextInput></TextInput>
-                        </View>
-                        </KeyboardAvoidingView>
-                    </View>
-                </ScrollView>
-            </GestureHandlerRootView> 
+                    <KeyboardAvoidingView>
+                        {isReg === true ? (
+
+                            <View style={styles.form}>
+                                <View style={styles.textInput}>
+                                    <Icon name="envelope" size={20} />
+                                    <TextInput style={{ width: "100%", height: 40 }} placeholder='.....@gmail.com'></TextInput>
+                                </View>
+                                <View style={styles.textInput}>
+                                    <Icon name="lock" size={20} />
+                                    <TextInput style={{ width: "100%", height: 40 }} placeholder='password'></TextInput>
+                                </View>
+                                <View style={styles.btnContainer}>
+                                    <TouchableOpacity style={styles.btn} >
+                                        <Text>Login</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        ) : (
+                            <Modal
+                                // visible={isReg === false}
+                                onRequestClose={() => setisReg(!isReg)}
+                                animationType="slide"
+                            >
+                                <View
+                                    style={{
+                                        backgroundColor: "#05992C",
+                                        flex: 1,
+                                        justifyContent: "flex-end",
+                                        alignItems: "center",
+                                        paddingHorizontal: 5,
+                                    }}
+                                >
+                                    <Ionicons name="medical" size={100} color="white" />
+                                    <UserRegistration />
+                                </View>
+                            </Modal>
+
+                        )
+
+                        }
+                    </KeyboardAvoidingView>
+                </View>
+            </ScrollView>
+        </GestureHandlerRootView>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         paddingHorizontal: 12,
     },
     title: {
@@ -37,13 +84,34 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         color: "white",
-        marginTop: height*0.05,
-        marginBottom:height*0.1
+        marginTop: height * 0.05,
+        marginBottom: height * 0.1
+    },
+    switchContainer: {
+        width: "100%",
+        marginTop: 20,
+        alignItems: "center",
+        flexDirection: "row",
+        columnGap: 5,
+        backgroundColor: "whitesmoke",
+        justifyContent: "space-evenly",
+        paddingHorizontal: 5,
+        height: 80
+    },
+    switchBtn: {
+        width: "45%",
+        height: 45,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        backgroundColor: "white"
+
     },
     formcontainer: {
-        height:height*0.8,
-        backgroundColor:"white",
-        borderRadius:10,
+        height: height * 0.8,
+        backgroundColor: "white",
+        borderRadius: 10,
+        padding: 20,
 
 
     },
@@ -52,5 +120,38 @@ const styles = StyleSheet.create({
         color: "white",
         fontWeight: "800",
     },
+    form: {
+        flexDirection: "column",
+        rowGap: 30,
+        marginTop: 70
+        //    backgroundColor:"black"
+    },
+    textInput: {
+        flexDirection: "row",
+        borderWidth: 1,
+        borderColor: "grey",
+        borderRadius: 10,
+        height: 50,
+        paddingHorizontal: 20,
+        alignItems: "center",
+        columnGap: 5
+    },
+    btnContainer: {
+        flexDirection: "row",
+        height: 80,
+        paddingHorizontal: 20,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    btn: {
+        borderWidth: 1,
+        width: "100%",
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+        backgroundColor: "rgb(6, 112, 179)"
+    }
+
 
 })
